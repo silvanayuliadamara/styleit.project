66349c26ffa4b36263d1749c914d5ef12214461d
Silvana Yulia Damara <silvanayuliadamara@gmail.com>
2026-06-11 11:38:07 +0700

    feat: menambahkan AdminDashboardController backend

---
+++ app/Http/Controllers/Admin/AdminBookingController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Booking;
+use Illuminate\Http\Request;
+
+class AdminBookingController extends Controller
+{
+    public function index()
+    {
+        $bookings = Booking::latest()->paginate(10);
+        return view('admin.bookings.index', compact('bookings'));
+    }
+
+    public function show(Booking $booking)
+    {
+        return view('admin.bookings.show', compact('booking'));
+
+    }
+
+    public function updateStatus(Request $request, Booking $booking)
+    {
+        $validated = $request->validate([
+            'status' => ['required', 'in:pending,confirmed,process,completed,cancelled,expired'],
+        ]);
+
+        $booking->update([
+            'status' => $validated['status'],
+        ]);
+
+        return redirect()->back()
+            ->with('succces','Status booking berhasil diperbarui.');
+    }
+
+
+}
