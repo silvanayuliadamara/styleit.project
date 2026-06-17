7da8d026e28a24fda6668210d00e54ddf74d5f2c
Silvana Yulia Damara <silvanayuliadamara@gmail.com>
2026-06-17 16:41:51 +0700

    feat: tambah field harga, status, relasi kategori dan opsi di model Addon

--- app/Models/Addon.php
+++ app/Models/Addon.php
@@ -6,10 +6,34 @@ use Illuminate\Database\Eloquent\Model;
 
 class Addon extends Model
 {
-    protected $fillable = ['name', 'description', 'price', 'is_active'];
+    protected $fillable = [
+        'name',
+        'description',
+        'price',
+        'is_active',
+        'harga_default',
+        'is_pihak_lain',
+        'biaya_pihak_lain',
+        'status'
+    ];
 
     protected $casts = [
         'price' => 'integer',
         'is_active' => 'boolean',
+        'harga_default' => 'integer',
+        'is_pihak_lain' => 'boolean',
+        'biaya_pihak_lain' => 'integer',
     ];
+
+    public function categories()
+    {
+        return $this->belongsToMany(ServiceCategory::class, 'category_addons', 'addon_id', 'category_id')
+            ->withPivot('status')
+            ->withTimestamps();
+    }
+
+    public function options()
+    {
+        return $this->hasMany(AddonOption::class);
+    }
 }
