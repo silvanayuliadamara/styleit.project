855f5ec6b6f597f60f3331ecd8999049bcc10604
Silvana Yulia Damara <silvanayuliadamara@gmail.com>
2026-06-17 16:45:24 +0700

    feat: tambah field pihak lain, biaya dan keterangan di model PackageItem

--- app/Models/PackageItem.php
+++ app/Models/PackageItem.php
@@ -7,7 +7,16 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class PackageItem extends Model
 {
-    protected $fillable = ['package_id', 'name', 'quantity', 'unit'];
+    protected $fillable = [
+        'package_id', 'name', 'quantity', 'unit',
+        'is_pihak_lain', 'biaya_pihak_lain', 'keterangan'
+    ];
+
+    protected $casts = [
+        'quantity' => 'integer',
+        'is_pihak_lain' => 'boolean',
+        'biaya_pihak_lain' => 'integer',
+    ];
 
     public function package(): BelongsTo
     {
