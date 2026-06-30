95ce70d8e9f878860e9878fadb60ea09f90f5b4a
Silvana Yulia Damara <silvanayuliadamara@gmail.com>
2026-06-30 22:24:11 +0700

    fix: Memperbaiki layout dashboard dan profil customer

--- resources/views/layouts/customer.blade.php
+++ resources/views/layouts/customer.blade.php
@@ -33,6 +33,7 @@
             background: #251313 !important;
         }
     </style>
+    @stack('styles')
 </head>
 
 <body class="lyb-admin-body">
@@ -99,6 +100,7 @@
         </main>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    @stack('scripts')
 </body>
 
 </html>
